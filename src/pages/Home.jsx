ec66acefce409047be6d8847cacdca84094fffc3
Delta27222 <angelgabriel.hernandez@gmail.com>
2022-08-22 10:07:25 -0700

    Voy por el vidio 17

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 //Importamos los componentes
-import Header from '../components/Header'
 import ProductList from '../containers/ProductList';
 
 const Home = () => {
 	return (
 		<>
-			<Header />
-			<h1>Voy por el vidio 15 del curso</h1>
+			<h1>Voy por el vidio 17 del curso</h1>
 			<ProductList />
 		</>
 	);
