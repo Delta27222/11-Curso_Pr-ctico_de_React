8380e971cfea304ca207e60de573beb67a9f931b
Delta27222 <angelgabriel.hernandez@gmail.com>
2022-08-22 10:05:35 -0700

    Adding icons's folder and logos's folder, and some configurations

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+//Importamos los componentes
+import Header from '../components/Header'
+
 const Layout = ({children}) => {
     return (
         <div className="Layout">
+            <Header />
             {children}
         </div>
     );
